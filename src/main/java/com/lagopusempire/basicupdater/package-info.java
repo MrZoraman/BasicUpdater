69ccb756aa35e115317b6b37b1570ed355ebcdf8
Zora <MrZoraman@Gmail.com>
2017-07-02 18:16:38 -0700

    A final bit of documentation

---
+++ src/main/java/com/lagopusempire/basicupdater/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains the basic classes for using the basic updater library.
+ * 
+ * @see com.lagopusempire.basicupdater.Updater
+ * @see com.lagopusempire.basicupdater.Update
+ */
+package com.lagopusempire.basicupdater;
