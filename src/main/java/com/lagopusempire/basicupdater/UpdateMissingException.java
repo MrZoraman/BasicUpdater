371c89ef319a0ffdad0635de2a0e14f0fbfd7274
Zora <MrZoraman@Gmail.com>
2017-07-02 18:20:31 -0700

    javadoc compiles now.

--- src/main/java/com/lagopusempire/basicupdater/UpdateMissingException.java
+++ src/main/java/com/lagopusempire/basicupdater/UpdateMissingException.java
@@ -3,9 +3,9 @@ package com.lagopusempire.basicupdater;
 /**
  * This exception is thrown if the updater has no way of procedurally updating
  * to a desired version. This means that there are updates that are missing.
- * For instance, if the updater knows about an update from A -> B, and an update
- * from C -> D, then this exception will be thrown if it is asked to go from
- * A -> D, because the updater is missing an update(s) to go from B -> C.
+ * For instance, if the updater knows about an update from A -&gt; B, and an update
+ * from C -&gt; D, then this exception will be thrown if it is asked to go from
+ * A -&gt; D, because the updater is missing an update(s) to go from B -&gt; C.
  * This is another way of saying the system never reached the required
  * prerequisite version needed for an update to run.
  * 
@@ -29,8 +29,8 @@ public class UpdateMissingException extends RuntimeException {
     
     /**
      * Gets the version that is missing the update. For example, if the updates
-     * A -> B and C -> D are present, then this would return version B, as
-     * there is no update from B -> C. As exceptions cannot
+     * A -&gt; B and C -&gt; D are present, then this would return version B, as
+     * there is no update from B -&gt; C. As exceptions cannot
      * use generics, it will be up to the library user to cast to the
      * correct generic type.
      * @return The last version the updater was able to get to before it reached
